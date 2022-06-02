df576d0ec49765ed5c83a4a2952409bdca355cc4
A.Badran <A.Badran@AREEN-BADRAN>
2022-06-02 18:24:19 +0300

    Setup the app

---
+++ Web form application/Web form application/Account/ResetPasswordConfirmation.aspx.cs
@@ -0,0 +1,8 @@
+﻿using System.Web.UI;
+
+namespace Web_form_application.Account
+{
+    public partial class ResetPasswordConfirmation : Page
+    {
+    }
+}
---
+++ Web form application/Web form application/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Web_form_application.Startup))]
+namespace Web_form_application
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
